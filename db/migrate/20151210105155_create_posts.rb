fcd36a5890a8d982cef8f8627d53e7dcbd53b853
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-10 11:02:41 -0800

    Add type

--- db/migrate/20151210105155_create_posts.rb
+++ db/migrate/20151210105155_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, :body
+      t.string :title, :body, :type
       t.integer :user_id, :vote_sum
 
       t.timestamps(null: false)
