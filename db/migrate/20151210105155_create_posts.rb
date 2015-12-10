3647e79acd0db71a9f6696f5b06ba6fdde5bac21
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-10 10:57:47 -0800

    Set up table columns

--- db/migrate/20151210105155_create_posts.rb
+++ db/migrate/20151210105155_create_posts.rb
@@ -1,4 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title, :body
+      t.integer :user_id, :vote_sum
+
+      t.timestamps(null: false)
+    end
   end
 end
