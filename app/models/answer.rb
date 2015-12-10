d60bd3b35881af273e992e46536e86db79999922
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-10 12:05:14 -0800

    Add associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < Post
   # Remember to create a migration!
+  has_many :comments, class_name: "Post",
+                      foreign_key: "question_id"
+  belongs_to :question, class_name: "Post"
 end
--- db/migrate/20151210105155_create_posts.rb
+++ db/migrate/20151210105155_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title, :body, :type
       t.integer :user_id, :vote_sum
+      t.references :question, :answer, index: true
 
       t.timestamps(null: false)
     end
