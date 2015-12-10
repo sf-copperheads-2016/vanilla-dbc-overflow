67834bc638b3d11d1bc7c8b1ffd868dbeec7e123
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-10 14:29:40 -0800

    Create post route for questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+get '/questions' do
+  # list questions
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  # form for question
+  erb :'questions/new'
+end
+
+post '/questions' do
+  # new question
+  @question = Question.new(title: params[:title],
+                           body: params[:body],)
+  if @entry.save
+    redirect "/questions/#{@question.id}"
+  else
+    # flash applicable error
+  end
+  # redirect "/questions"
+end
+
+get '/questions/:id' do
+  # specific question
+
+  erb :"/questions/show"
+end
+
+get '/questions/:id/edit' do
+  # edit form
+  erb :"/questions/edit"
+end
+
+put 'questions/:id' do
+  # update question
+  redirect "/questions/#{params[:id]}"
+end
+
+delete 'questions/:id' do
+  # delete question
+  redirect '/questions'
+end
