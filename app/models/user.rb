be6ef5d86100a595e68837c4b3607384fa0626ac
Kim Allen & Kyle Smith <pair+kimallen.kylefantastic@devbootcamp.com>
2015-12-11 10:53:11 -0800

    Finish auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,17 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :votes
   has_many :posts
-
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true, length: { minimum: 1}
+  validates :name, presence: true
   def password
     @password ||= BCrypt::Password.new(password_hash)
   end
 
   def password=(new_password)
-    @password = BCrypt::Password.create(new_password)
-    self.password_hash = @password
+    unless new_password == ""
+      @password = BCrypt::Password.create(new_password)
+      self.password_hash = @password
+    end
   end
 end
