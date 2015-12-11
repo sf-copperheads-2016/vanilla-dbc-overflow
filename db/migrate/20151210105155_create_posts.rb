a4fe3cbdfc17b3276e4bd49c5643163e8c7f7a17
Nicole Yee <n.lindsey.yee@gmail.com>
2015-12-11 15:20:48 -0800

    Add best answer column, change seed file to reflect changes

--- db/migrate/20151210105155_create_posts.rb
+++ db/migrate/20151210105155_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
-      t.string :title, :body, :type
-      t.integer :user_id, :vote_sum
+    create_table   :posts do |t|
+      t.string     :title, :body, :type
+      t.integer    :user_id, :vote_sum
       t.references :question, :answer, index: true
-      t.boolean :best_answer
+      t.boolean    :best_answer, default: false
 
       t.timestamps(null: false)
     end
